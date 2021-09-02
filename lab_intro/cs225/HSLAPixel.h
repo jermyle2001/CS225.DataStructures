24afae7d563a23eafc9eacb7228d946bfafaafa8
Jeremy Lee <jeremyl6@illinois.edu>
2021-09-02 00:19:11 -0500

    Finished lab_intro at 12:18 AM 9/2/2021

--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,16 @@
 #include <sstream>
 
 namespace cs225 {
+ class HSLAPixel {
+  public:
+	HSLAPixel();
+	HSLAPixel(double hue, double saturation, double luminance);
+	HSLAPixel(double hue, double saturatioh, double luminance, double alpha);
+	double h; //Hue value for the new pixel, in degree [0,360)
+	double s; //Saturation value for the new pixel, [0,1]
+	double l; //Luminance value for the new pixel, [0,1]
+	double a; //Alpha value for the new pixel, [0,1]
+
+
+ };
 }
--- lab_intro/lab_intro.cpp
+++ lab_intro/lab_intro.cpp
@@ -14,6 +14,17 @@
 using cs225::HSLAPixel;
 using cs225::PNG;
 
+/* ITERATION TEMPLATE
+ for(unsigned x = 0; x < image.width(); x++){ //Iterate throughout PNG's width
+  for(unsigned y = 0; y < image.height(); y++){ //Iterate through PNG's height
+   HSLAPixel & pixel = image.getPixel(x, y); //Get pixel information
+
+  }
+ }
+
+
+
+*/
 /**
  * Returns an image that has been transformed to grayscale.
  *
@@ -61,9 +72,21 @@ PNG grayscale(PNG image) {
  * @return The image with a spotlight.
  */
 PNG createSpotlight(PNG image, int centerX, int centerY) {
-
+ //Loop through all of the pixels of the PNG (width x height)
+ for (unsigned int x = 0; x < image.width(); x++){ //Iterate through width of PNG
+  for(unsigned int y = 0; y < image.height(); y++){ //Iterate through height of PNG
+  HSLAPixel & pixel = image.getPixel(x, y); //Get pixel information
+  double distance = sqrt((centerX - x) * (centerX - x) + (centerY - y) * (centerY - y)); //Calculate distance from center
+  if(distance > 160){ //If distance > 160, then we need to reduce luminance to 20% by default)
+   pixel.l = 0.2 * pixel.l; //Reduce luminance to 20% of original (decrease by 80%)
+   }
+  else{ //Else, we reduce luminance of pixel by 0.5% per 1 pixel away 
+   pixel.l = pixel.l * (1.0 - (0.005 * distance)); //Convert reduction to a percentage via (0.5 * distance)/100, subtract from 1 to get multiplier, apply to pixel 
+   } 
+  }
+ }
   return image;
-  
+ 
 }
  
 
@@ -78,13 +101,28 @@ PNG createSpotlight(PNG image, int centerX, int centerY) {
  * @return The illinify'd image.
 **/
 PNG illinify(PNG image) {
+ for(unsigned x = 0; x < image.width(); x++){ //Iterate throughout PNG's width
+  for(unsigned y = 0; y < image.height(); y++){ //Iterate through PNG's height
+   HSLAPixel & pixel = image.getPixel(x, y); //Get pixel information
+   /* if(abs(pixel.h - 11) < abs(pixel.h - 216)){ //If the hue is closer to orange than blue, then...
+    pixel.h = 11; //Hue closer to orange than blue. Set to orange.
+   }
+   else{
+    pixel.h = 216; //Hue closer to blue than orange. Set to blue.
+   } */
+    if(pixel.h > 320.5 || pixel.h <= 113.5) //Orange's hue values are between (320.5, 360]U[0, 140.5]. Test if either is true.
+     pixel.h = 11;
+    else
+     pixel.h = 216;
+  }
+ }
 
   return image;
 }
  
 
 /**
-* Returns an immge that has been watermarked by another image.
+* Returns an image that has been watermarked by another image.
 *
 * The luminance of every pixel of the second image is checked, if that
 * pixel's luminance is 1 (100%), then the pixel at the same location on
@@ -96,6 +134,18 @@ PNG illinify(PNG image) {
 * @return The watermarked image.
 */
 PNG watermark(PNG firstImage, PNG secondImage) {
-
+ for(unsigned x = 0; x < secondImage.width(); x++){ //Iterate throughout PNG's width
+  for(unsigned y = 0; y < secondImage.height(); y++){ //Iterate through PNG's height
+   HSLAPixel & pixel = secondImage.getPixel(x, y); //Get pixel information of second image
+   if(pixel.l == 1){ //Check if luminance of second image is 1 (100%) - otherwise, do nothing
+    HSLAPixel & pixel = firstImage.getPixel(x, y); //Get pixel information of first image
+    if((pixel.l + 0.2) <= 1)
+     pixel.l = pixel.l + 0.2; //Increase luminance on first image by 0.2 (within bounds)
+    else //Bounds exceeded when luminance increased, set luminance to max (1)
+     pixel.l = 1; 
+   }
+    
+  }
+ }
   return firstImage;
 }
