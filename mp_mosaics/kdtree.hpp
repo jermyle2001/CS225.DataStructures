8adae8819ab751b34e66e8d74aa4a7f35477ac0a
Jeremy Lee <jeremyl6@illinois.edu>
2021-10-25 23:46:47 -0500

    Committing partially finished mp_mosaics part 1 11:46PM 10/25/2021

--- mp_mosaics/kdtree.hpp
+++ mp_mosaics/kdtree.hpp
@@ -138,24 +138,175 @@ Point<Dim> KDTree<Dim>::findNearestNeighbor(const Point<Dim>& query) const
 
     /*
       The findNearestNeighbor functions as follows:
-        The function recurisvely traverses down the tree. At each level, the function must choose 
+        The function recursively traverses down the tree. At each level, the function must choose 
         the subtree that contains the search element. It will recurse right or left based on the 
         splitting dimension of the current level.
         Once the lowest-bound hyperrectangle is reached, the node in that rectangle is effectively the 
-        "current best" neighbor
+        "current best" neighbor.
 
+        Afterwards, the function will begin traversing back up the tree to the parent node, checking to see if
+          any other nodes along the way would be better matches for our element. We can compare distances, or radii
+          to the parent node, to see if the new radii is smaller than the old one. If so, we replace the radii and 
+          "current best" point/match.
+
+        Next, we need to check to see if the "current splitting plane's distance from search node is within the
+          current radius". Essentially, we check to see if our plane's distance from the search node is low enough
+          to be considered, as thus we would have to consider the opposite subtree and search it recursively.
+          (Note: this is how I interpret it, it may or may not be correct).
+
+        Additionally, during back-tracking we only consider subtrees within the current radius, as checking
+          subtrees with larger radii is pointless and inefficient.
     */
 
+    //Note: we need to create a helper function to pass through the appropriate variables for recursion.
+    //  It will be included directly below.
 
     return Point<Dim>();
 }
 
-
-
-
 /*
   Helper functions included below!
 */
+
+//Below is the helper function for findNearestNeighbor
+template <int Dim>
+Point<Dim> KDTree<Dim>::FNNHelper(const Point<Dim> & query, KDTreeNode* subRoot, int dimension)
+{
+  /*
+    We pass through the subroot we look at, the point we wish to find the closest neighbor to ("query"), and 
+      the dimension we're currently analyzing.
+  */
+
+  //First we create a point which is our subroot's point. We will use this in comparison later.
+  Point<Dim> currentBest = subRoot->point;
+  //Check to see if current our current point has any children/bounds are valid - if not, then we
+  //  can recurse with our remaining point, which may not necessarily be the best point.
+  if(subRoot->left == NULL && subRoot->right == NULL){
+    return subRoot->point;
+  }
+
+  /*
+    Next, we decide whether or not we should traverse each tree. Specifically, we will consider whether 
+      the left tree is worth traversing first, and then the right. We compare with respect to our current
+      dimension.
+
+    Additionally, we create a "flag" to help us determine which direction we have traverse in. This flag will 
+      vary b/w 1 and 0, with 1 representing "true" or we have traversed the left tree, and 0 being otherwise.
+      The flag will be used later on when checking for opposite subtrees to point us in the right direction of
+      traversal. As you read this code the role of the flag will become clearer.
+  */
+  int leftFlag = 0;
+  if(smallerDimVal(query, currentBest, dimension) == true){
+    /*
+      Comparing the values at our current dimension with smallerDimVal, we determine whether or not we want to 
+        traverse the left subtree. If the currentBest is smaller, then we do not traverse the left. Otherwise, 
+        we traverse left.
+    */
+    //We need to check if the left subtree even exists, so...
+    if(subRoot->left == NULL){ //If left subtree does NOT exist, then we traverse right
+      /*
+        We want to pass through our same query, but this time we iterate/traverse down our tree with the right side.
+          We also need to increment the dimension but keep it within the bounds of Dim.
+      */
+      currentBest = FNNHelper(query, subRoot->right, (dimension + 1) % Dim);
+      //leftFlag = 0;
+    }
+    else{ //Otherwise, we traverse down the left
+      currentBest = FNNHelper(query, subRoot->left, (dimension + 1) % Dim);
+      //Since we're traversing left, we also need to set our flag to be true
+      //leftFlag = 1;
+    }
+    leftFlag = 1;
+  }
+  else { //Else, we decide to traverse right
+    //Check to see if right side even exists
+    if(subRoot->right == NULL){
+      //Right subroot does not exist, so traverse left
+      currentBest = FNNHelper(query, subRoot->left, (dimension + 1) % Dim);
+      //leftFlag = 1;
+    }
+    else{
+      //Right subroot exists, so traverse right
+      currentBest = FNNHelper(query, subRoot->right, (dimension + 1) % Dim);
+      //leftFlag = 0;
+    }
+    leftFlag = 0;
+  }
+
+  /*
+    At this point, we have found the "current best" neighbor, or the point bound by the lowest-bound 
+      hyperrectangle. We now work our way back up and keep comparing distances using Euclidian Distance.
+
+    Currently, currentBest is the lowest-bound point in the hyperrectangle, but not necessarily the actual 
+      "best" point for our query. However, since we're traversing back upwards we compare our "current best"
+      to the point stored in our subRoot, which is effectively our "current point".
+  */
+
+  if(shoudReplace(query, currentBest, subRoot->point) == true){
+    //We replace our "current best" with our "current point" if we find it to be necessary
+    currentBest = subRoot->point;
+  }
+
+  //Now we want to calculate the radius of our point to compare it with any other points. Achieve this using
+  //  Euclidian distance method
+  int radius = 0;
+  //Iterate through each dimension, get the square of the distance in that dimension to calculate the radius
+  //  of that dimension
+  for(int i = 0; i < Dim; i++){
+    radius = radius + (query[i] - currentBest[i]) * (query[i] - currentBest[i]);
+  }
+
+  /*
+    We also want to calculate the splitting distance to check if it could contain another subtree. If the splitting
+      distance is lower than the radius, then we also need to check the opposite subtree to see if any points
+      would be more viable.
+  */
+
+  auto splitDistance = (subRoot->point[dimension] - query[dimension]) * (subRoot->point[dimension] - query[dimension]);
+
+  /*
+    Now we check if the splitDistance is less than the radius. If that's true, then subtree contained within the
+      splitDistance could hold better points, and we woud need to search those trees too.
+  */
+  if(splitDistance <= radius){
+    /*
+      This is where our flag comes in handy: we use it to check which subtree we have already traversed, and
+        traverse the opposite subtree instead. We create a pointer to our new subtree called "newSubTree", and
+        set it according to our flag.
+    */
+    KDTreeNode* newSubTree = NULL;
+    if(leftFlag == 1){ 
+      /*
+        If this is true, that means we previously traversed down the left side. Perform the same checks and 
+          traverse recursively.
+      */
+      newSubTree = subRoot->right;
+    }
+    else{
+      newSubTree = subRoot->left;
+    }
+
+    //We need to check if the newSubTree is NULL. If so, then we can just return currentBest, as there are no other
+    //  subtrees to check and currentBest is the best possible point.
+    if(newSubTree == NULL){
+      return currentBest;
+    }
+    else{ 
+      //newSubTree is NOT null, indicating that another subTree exists, thus we need to traverse that subTree and
+      //  get whatever point is the most viable out of it using our helper function's recursive functionality.
+      Point<Dim> otherTreeBest = FNNHelper(query, newSubTree, (dimension + 1) % Dim);
+      //Now that we've gotten the another viable point, we finally compare our two points and see if 
+      //  the new point is more viable than the first point.
+      if(shouldReplace(query, currentBest, otherTreeBest) == true){
+        currentBest = otherTreeBest;
+      }
+    }
+    return currentBest;
+  }
+
+} //EoF
+
+
 
 template <int Dim>
 unsigned KDTree<Dim>::partition(vector<Point<Dim>>& list, unsigned dimension, unsigned left, unsigned right, unsigned pivotIndex)
